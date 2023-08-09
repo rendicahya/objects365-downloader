3ced61119c9531e94a9c6b504a6f734a347917f6
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-08-09 10:03:37 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,9 +31,9 @@ echo "  Done"
 
 echo "Extracting train images..."
 for file in "$ROOT_DIR/images-temp/train"/*.tar.gz; do
-    echo "  Extracting $file..."
+    echo "  Extracting $file"
     tar -xzf "$file" -C "$ROOT_DIR/images/train"
-    echo "    Done"
+    echo "    Done."
 
     if [ "$remove_temp" == "y" ] || [ "$remove_temp" == "Y" ]; then
         rm "$file"
@@ -43,67 +43,67 @@ done
 
 echo "Extracting validation images..."
 for file in "$ROOT_DIR/images-temp/val"/*.tar.gz; do
-    echo "  Extracting $file..."
+    echo "  Extracting $file"
     tar -xzf "$file" -C "$ROOT_DIR/images/val"
-    echo "    Done"
+    echo "    Done."
 
     if [ "$remove_temp" == "y" ] || [ "$remove_temp" == "Y" ]; then
         rm "$file"
-        echo "  $file deleted"
+        echo "  $file deleted."
     fi
 done
 
-echo "Arranging train images..."
+echo "Setting up train images..."
 find "$ROOT_DIR/images/train" -name "*.jpg" -exec mv -t "$ROOT_DIR/images/train" {} +
-echo "  Done"
+echo "  Done."
 
-echo "Arranging validation images..."
+echo "Setting up validation images..."
 find "$ROOT_DIR/images/val" -name "*.jpg" -exec mv -t "$ROOT_DIR/images/val" {} +
-echo "  Done"
+echo "  Done."
 
 echo "Removing empty directories..."
 find "$ROOT_DIR/images/train" -type d -empty -delete
 find "$ROOT_DIR/images/val" -type d -empty -delete
-echo "  Done"
+echo "  Done."
 
 echo "Extracting train labels..."
 unzip labels-train.zip -d labels
-echo "  Done"
+echo "  Done."
 
 echo "Extracting validation labels..."
 unzip labels-val.zip -d labels
-echo "  Done"
+echo "  Done."
 
 echo "Counting train images..."
 num_train_img=$(find "$ROOT_DIR/images/train" -type f -name "*.jpg" | wc -l)
 size_train_img=$(du -sh "$ROOT_DIR/images/train" | cut -f 1)
-echo "  Done"
+echo "  Done."
 
 echo "Counting validation images..."
 num_val_img=$(find "$ROOT_DIR/images/val" -type f -name "*.jpg" | wc -l)
 size_val_img=$(du -sh "$ROOT_DIR/images/val" | cut -f 1)
-echo "  Done"
+echo "  Done."
 
 echo "Counting train labels..."
 num_train_lbl=$(find "$ROOT_DIR/labels/train" -type f -name "*.txt" | wc -l)
 size_train_lbl=$(du -sh "$ROOT_DIR/labels/train" | cut -f 1)
-echo "  Done"
+echo "  Done."
 
 echo "Counting validation labels..."
 num_val_lbl=$(find "$ROOT_DIR/labels/val" -type f -name "*.txt" | wc -l)
 size_val_lbl=$(du -sh "$ROOT_DIR/labels/val" | cut -f 1)
-echo "  Done"
+echo "  Done."
+
+if [ "$remove_temp" == "y" ] || [ "$remove_temp" == "Y" ]; then
+    echo "Deleting the temporary directories..."
+    rm -rf "$ROOT_DIR/images-temp"
+    echo "  Done."
+else
+    echo "Temporary directories not deleted."
+fi
 
 echo "[SUMMARY]"
 echo "$num_train_img train images. Total size: $size_train_img"
 echo "$num_val_img val images. Total size: $size_val_img"
 echo "$num_train_lbl train labels. Total size: $size_train_lbl"
 echo "$num_val_lbl val labels. Total size: $size_val_lbl"
-
-if [ "$remove_temp" == "y" ] || [ "$remove_temp" == "Y" ]; then
-    echo "Deleting the temporary directories..."
-    rm -rf "$ROOT_DIR/images-temp"
-    echo "Temporary directories deleted."
-else
-    echo "Temporary directories not deleted."
-fi
